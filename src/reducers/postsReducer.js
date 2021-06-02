674e0b0d5dd0d0bd044a01054aed86f882166d29
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-02 22:16:37 +0530

    rendering postlist

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,14 +1,10 @@
-export default (state = [],action) => {
-    // if(action.type === 'FETCH_POST') {
-    //   return action.payload;
-    // }
-    // return state;
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
-    switch (action.type) {
-      case 'FETCH_POSTS':
-        return action.payload;
-      default:
-        return state;
-    }
-  };
   
