a80efd2a7c2a644559c244ee201aea82772b6e81
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-02 19:35:28 +0530

    fixxed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,17 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    };
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
+
+
+// BAD APPROACH
+// export const fetchPosts = async () => {
+//     const response = await jsonPlaceholder.get('/posts')
+//     return {
+//         type: 'FETCH_POSTS',
+//         payload: response
+//     };
+// };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {fetchPosts} from '../actions';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
-    componentDidMount() {
-        this.props.fetchPosts();
-    }
-    
-    render() {
-        return (
-            <div>Post List</div>
-        )
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return <div>Post List</div>;
+  }
 }
 
-export default connect(null, {
-    fetchPosts,
-})(PostList);
+export default connect(
+  null,
+  { fetchPosts }
+)(PostList);
