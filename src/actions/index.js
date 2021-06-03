7315782777b61e55bcf1608b1feb15859333c1e8
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-03 21:11:04 +0530

    alterate way to minimize network request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,30 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash'
 
+export const fetchPostAndUser = () => async (dispatch, getState) => {
+ await dispatch(fetchPosts());
+ const userId = _.uniq(_.map(getState().posts,'userId'))
+ userId.forEach(id=>dispatch(fetchUser(id)));
+}
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// export const fetchUser = (id) => async dispatch => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
-
-//   dispatch({type: 'FETCH_USER', payload: response.data})
-// };
-
-export const fetchUser = id => dispatch => _fetchUser(id,dispatch);
-const _fetchUser = _.memoize(async (id,dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-    dispatch({ type: 'FETCH_USER', payload: response.data })
-});
+  dispatch({type: 'FETCH_USER', payload: response.data})
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id,dispatch);
+// const _fetchUser = _.memoize(async (id,dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// });
 
 
 // BAD APPROACH
